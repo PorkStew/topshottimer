a8fbf31210f7c46c32f4b970b664a8df5efb5629
stewart-clay-simplx <stewart@simplx.co.za>
2020-11-02 09:59:48 +0200

    adding data

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,162 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart';
+import 'package:topshottimer/signup.dart' as signup;
+import 'package:flutter_session/flutter_session.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  //root widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Top Shot Timer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login() ,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Login extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return LoginState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class LoginState extends  State<Login> {
+  String _email;
+  String _password;
+  final email = TextEditingController();
+  final passwords = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  //email widget
+  Widget _buildEmail() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Email'),
+      controller: email,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Email is Required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _email = value;
+      },
+    );
+  }
+
+  //password widget
+  Widget _buildPassword() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Password'),
+      obscureText: true,
+      // maxLength: 10,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Password is Required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _password = value;
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: AppBar(title: Text("Login")),
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        margin: EdgeInsets.all(24),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildEmail(),
+              _buildPassword(),
+              SizedBox(height: 30),
+              RaisedButton(
+                child: Text('Submit',
+                  style: TextStyle(color: Colors.blue, fontSize: 16),
+
+                ),
+                onPressed: () {
+                  if (!_formKey.currentState.validate()) {
+                    return;
+                  }
+                  _formKey.currentState.save();
+                  print(_email);
+                  print(_password);
+                  print("Hello World");
+                  saveData(context);
+                  //Send to API
+                  updateData(_email, _password);
+                },
+              ),
+              RaisedButton(
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.blue, fontSize: 16),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => signup.FormScreen(email.text)));
+                  }
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future updateData(String email, String password) async {
+    String hashedPassword = "";
+    var bytes = utf8.encode(password);
+    var digest = sha256.convert(bytes);
+    hashedPassword = digest.toString();
+    var url = 'https://nutant-ratings.000webhostapp.com/read.php';
+    var res = await http.post(
+        Uri.encodeFull(url), headers: {"Accept": "application/jason"},
+        body: {
+          "emailAddress": email,
+          "password": hashedPassword,
+        }
+    );
+    print("before res.body");
+    var data = json.decode(res.body);
+
+    print(data);
+    if (data != false) {
+      print("Success the details provided are correct");
+      // go to home screen
+      Navigator.push(context, MaterialPageRoute(builder: (context) => signup.FormScreen("something good")));
+    } else {
+      //print a message if details are incorrect
+      print("incorrect details! Please Try Again");
+    }
+  }
+  Future<void> saveData(context) async {
+    print("token");
+    var session = FlutterSession();
+    await session.set('id', '111111');
+    dynamic token = await FlutterSession().get("id");
+    print("printing token for you");
+    print(token);
+    //Navigator.push(context, MaterialPageRoute(builder: (context) => signup.FormScreen("something good")));
   }
 }
---
+++ lib/signup.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+import 'package:flutter_session/flutter_session.dart';
+class FormScreen extends StatefulWidget {
+  //allows the accepting of data from another view
+  String something;
+  FormScreen(this.something);
+
+  @override
+  State<StatefulWidget> createState() {
+    //need to accept a aurgement
+    return FormScreenState(this.something);
+  }
+}
+
+class FormScreenState extends  State<FormScreen> {
+  String _firstName;
+  String _lastName;
+  String _email;
+  String _password;
+  String _conPassword;
+  final passwords = TextEditingController();
+  final conPasswords = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  //allows the accepting of data from another view
+  String emailFromLogin;
+  FormScreenState(this.emailFromLogin);
+
+  Widget _buildFirstName() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'First name here'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          saveData(context);
+          return 'First name is Required';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _firstName = value;
+      },
+    );
+  }
+  Widget _buildLastName() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Last Name'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Last name is Required';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _lastName = value;
+      },
+    );
+  }
+  //email text area
+  Widget _buildEmail() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Email'),
+      initialValue: emailFromLogin,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Email is Required';
+        }
+        //regex
+        if (!RegExp(
+            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            .hasMatch(value)) {
+          return 'Please enter a valid email Address';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _email = value;
+      },
+    );
+  }
+  //password text area
+  Widget _buildPassword() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Password'),
+      obscureText: true,
+      controller: this.passwords,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Last name is Required';
+        }
+        //regex to check strength of password
+        if (!RegExp(
+            r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$")
+            .hasMatch(value)) {
+          return 'Please enter a valid password';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _password = value;
+      },
+    );
+  }
+  //confirm password text area
+  Widget _buildConPassword() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Password'),
+      obscureText: true,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Password cant be empty';
+        }
+        //checks if passwords are matching
+        if(value != passwords.text){
+          print("passwords dont match");
+          print(value);
+          print(passwords.text);
+        }
+
+        if (!RegExp(
+            r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$")
+            .hasMatch(value)) {
+          return 'Password not strong';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _conPassword = value;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sign Up")),
+      body: Container(
+        margin: EdgeInsets.all(24),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildFirstName(),
+              _buildLastName(),
+              _buildEmail(),
+              _buildPassword(),
+              _buildConPassword(),
+              SizedBox(height: 100),
+              RaisedButton(
+                child: Text(
+                  'Submit',
+                  style: TextStyle(color: Colors.blue, fontSize: 16),
+                ),
+                onPressed: () {
+                  if (!_formKey.currentState.validate()) {
+                    return;
+                  }
+                  _formKey.currentState.save();
+                  print(_firstName);
+                  print(_lastName);
+                  print(_email);
+                  print(_password);
+                  print(_conPassword);
+                  //Send to API
+                  //send user information to the database
+                  sendData(_firstName, _lastName, _email, _password);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Future sendData(String firstName, String lastName, String email, String password) async {
+    String hashedPassword = "";
+    var bytes = utf8.encode(password);
+    var digest = sha256.convert(bytes);
+    hashedPassword = digest.toString();
+    print("Digest as hex string: $digest");
+    var de = utf8.decode(bytes);
+    print(de);
+    try{
+      var url = 'https://nutant-ratings.000webhostapp.com/create.php';
+      var res = await http.post(
+          Uri.encodeFull(url), headers: {"Accept": "application/jason"},
+          body: {
+            "firstName": firstName,
+            "lastName": lastName,
+            "emailAddress": email,
+            "password": hashedPassword,
+            "country": "delete",
+            "contactNumber": "delete",
+          }
+      );
+      print("account created");
+    }catch (error) {
+      print(error.toString());
+    }
+  }
+  Future<void> saveData(context) async {
+    dynamic token = await FlutterSession().get("id");
+    print(token);
+
+  }
+}
